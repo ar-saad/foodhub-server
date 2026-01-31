e8dc6932701b006f8fa4f7864f3c7b7ddb027aed
ar-saad <arsaad.dev@gmail.com>
2026-01-31 09:19:24 +0600

    feat: add emoji and image fields to Category model; add isFeatured field to Meal model

---
+++ prisma/migrations/20260131031807_add_emoji_and_image_field_to_category_table_and_add_is_featured_field_to_meal_table/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - Added the required column `emoji` to the `category` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `image` to the `category` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "category" ADD COLUMN     "emoji" TEXT NOT NULL,
+ADD COLUMN     "image" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "meal" ADD COLUMN     "isFeatured" BOOLEAN NOT NULL DEFAULT false;
